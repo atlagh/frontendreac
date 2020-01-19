8dea3a988764a0fd6e4c853481e81e7b799062ee
amine <atlagh.amine@gmail.com>
2020-01-19 11:56:56 +0100

    search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBar from './SearchBar';
 import {friends} from './friends';
 
-const App = () =>{
-	return(
-		<div className='tc'>
-			<h1>Robot friends</h1>
-			<SearchBar />
-			<CardList friends={friends}/>
-		</div>
-	);
+
+class App extends Component{
+	constructor (){
+		super()
+		this.state = { 
+			friends: friends,
+			searchfield: ''
+		}
+
+	}
+
+	onSearchChange = (event) => { 
+		this.setState({searchfield: event.target.value})
+	}
+	
+	render () {
+		
+		const filterFriends = this.state.friends.filter(friends=>{
+			return friends.name.toLowerCase().includes(this.state.searchfield.toLowerCase())})
+		
+		return(
+			<div className='tc'>
+				<h1>Robot friends</h1>
+				<SearchBar searchChange={this.onSearchChange}/>
+				<CardList friends={filterFriends}/>
+			</div>
+		);
+
+	}
+	
 }
 
 export default App;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-const SearchBar = ()=> {
+const SearchBar = ({searchfield, searchChange})=> {
 	return(
 		<div>
 			<input 
 				className='pa3 ba b--green bg-lightest-blue'
 				type='serach'
 				placeholder='search robots'
+				onChange={searchChange}
 			/>
 		</div>
 	);
