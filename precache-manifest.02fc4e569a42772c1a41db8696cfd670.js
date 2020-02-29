d98b61692c736681e394f39fa397bad7ec10c44a
amine <atlagh.amine@gmail.com>
2020-02-29 17:32:18 +0100

    Updates

---
+++ precache-manifest.02fc4e569a42772c1a41db8696cfd670.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ae4c95ba8c3938af522572e5567b4e",
+    "url": "/frontendreac/index.html"
+  },
+  {
+    "revision": "f9924d5b13d0e388f36c",
+    "url": "/frontendreac/static/css/2.f8399ec5.chunk.css"
+  },
+  {
+    "revision": "9918383804c7c310f9b1",
+    "url": "/frontendreac/static/css/main.b2796e8f.chunk.css"
+  },
+  {
+    "revision": "f9924d5b13d0e388f36c",
+    "url": "/frontendreac/static/js/2.da171a10.chunk.js"
+  },
+  {
+    "revision": "e9ece340db509f2bc1b72d3278707526",
+    "url": "/frontendreac/static/js/2.da171a10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9918383804c7c310f9b1",
+    "url": "/frontendreac/static/js/main.aedc707e.chunk.js"
+  },
+  {
+    "revision": "3aa2ebe20fb75b21ce1c",
+    "url": "/frontendreac/static/js/runtime-main.bc2c8c7b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/frontendreac/static/media/SEGA.12a3db8b.woff"
+  }
+]);
